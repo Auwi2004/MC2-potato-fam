a17aa2fc2150bd984506f15e623ee4cc9ab3222c
Auwi2004 <puriaprianty@yahoo.com>
2021-06-13 10:51:59 +0700

    Initial Commit

---
+++ MC2 potato fam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MC2 potato fam
+//
+//  Created by Puri Aprianty on 13/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
