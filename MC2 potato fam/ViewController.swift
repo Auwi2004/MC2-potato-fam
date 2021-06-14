c4afefc769510ac63914f3723e042385ae3dc7a6
Catharina Benita <benita_irawan@yahoo.co.id>
2021-06-14 21:54:44 +0700

    cobacoba tambah story board lagi

--- MC2 potato fam/ViewController.swift
+++ MC2 potato fam/ViewController.swift
@@ -16,19 +16,19 @@ class ViewController: UIViewController {
 
 
 }
+//
+//class Core {
+//
+//    static let shared = Core()
+//
+//    func isNewUser() -> Bool {
+//        return !UserDefaults.standard.bool(forKey: "isNewUser")
+//
+//    }
+//
+//    ///
+//    func setIsNotNewUser() {
+//        UserDefaults.standard.set(true, forKey: "isNewUser")
+//}
 
-class Core {
-    
-    static let shared = Core()
-    
-    func isNewUser() -> Bool {
-        return !UserDefaults.standard.bool(forKey: "isNewUser")
-        
-    }
-    
-    ///
-    func setIsNotNewUser() {
-        UserDefaults.standard.set(true, forKey: "isNewUser")
-}
 
-}
