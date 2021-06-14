f96a053bfe38f1022f2c8bdb84e1844d65c2b3de
Catharina Benita <benita_irawan@yahoo.co.id>
2021-06-14 18:51:00 +0700

    coba-coba on boarding page & nambahin view controller

--- MC2 potato fam/ViewController.swift
+++ MC2 potato fam/ViewController.swift
@@ -17,3 +17,18 @@ class ViewController: UIViewController {
 
 }
 
+class Core {
+    
+    static let shared = Core()
+    
+    func isNewUser() -> Bool {
+        return !UserDefaults.standard.bool(forKey: "isNewUser")
+        
+    }
+    
+    ///
+    func setIsNotNewUser() {
+        UserDefaults.standard.set(true, forKey: "isNewUser")
+}
+
+}
